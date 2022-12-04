379ba8f722654a6a2b0fba295e6f062dfbd130e4
AriAliansah853 <aliansah628@gmail.com>
2022-12-04 17:33:44 +0700

    my-cv

---
+++ application/views/new_include/include_basecss.php
@@ -0,0 +1,18 @@
+<?php $this->load->view('new_include/include_header'); ?>
+<!-- Favicons -->
+<link href="<?php echo base_url(); ?>assets/new_porto/img/favicon.png" rel="icon">
+<link href="<?php echo base_url(); ?>assets/new_porto/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/new_porto/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url(); ?>assets/new_porto/css/style.css" rel="stylesheet">
---
+++ application/views/new_include/include_basejs.php
@@ -0,0 +1,13 @@
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/aos/aos.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/typed.js/typed.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="<?php echo base_url(); ?>assets/new_porto/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url(); ?>assets/new_porto/js/main.js"></script>
